328a71e442167eec86b4e17456f16bcb3addf68b
Ian Ker-Seymer <i.kerseymer@gmail.com>
2020-04-03 17:14:53 -0400

    Various upgrades

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #[macro_use]
-extern crate ruru;
+extern crate rutie;
 extern crate woothee;
 
-use ruru::{Symbol, Boolean, RString, Class, Hash, Object};
+use rutie::{Symbol, Boolean, RString, Class, Hash, Object};
 
 use woothee::parser::Parser;
 
@@ -15,16 +15,16 @@ methods!(
         let mut hash = Hash::new();
 
         let parser = Parser::new();
-        match parser.parse(ua_string.unwrap_or(RString::new("")).to_str()) {
+        match parser.parse(ua_string.unwrap_or(RString::new_usascii_unchecked("")).to_str()) {
             None => hash,
             Some(result) => {
-                hash.store(Symbol::new("name"), RString::new(&result.name));
-                hash.store(Symbol::new("category"), RString::new(&result.category));
-                hash.store(Symbol::new("os"), RString::new(&result.os));
-                hash.store(Symbol::new("os_version"), RString::new(&*result.os_version));
-                hash.store(Symbol::new("browser_type"), RString::new(&result.browser_type));
-                hash.store(Symbol::new("version"), RString::new(&*result.version));
-                hash.store(Symbol::new("vendor"), RString::new(&result.vendor));
+                hash.store(Symbol::new("name"), RString::new_usascii_unchecked(&result.name));
+                hash.store(Symbol::new("category"), RString::new_usascii_unchecked(&result.category));
+                hash.store(Symbol::new("os"), RString::new_usascii_unchecked(&result.os));
+                hash.store(Symbol::new("os_version"), RString::new_usascii_unchecked(&*result.os_version));
+                hash.store(Symbol::new("browser_type"), RString::new_usascii_unchecked(&result.browser_type));
+                hash.store(Symbol::new("version"), RString::new_usascii_unchecked(&*result.version));
+                hash.store(Symbol::new("vendor"), RString::new_usascii_unchecked(&result.vendor));
                 hash
             }
         }
@@ -35,7 +35,7 @@ methods!(
     FastWoothee,
     _itself,
     fn is_crawler(ua_string: RString) -> Boolean {
-        Boolean::new(woothee::is_crawler(ua_string.unwrap_or(RString::new("")).to_str()))
+        Boolean::new(woothee::is_crawler(ua_string.unwrap_or(RString::new_usascii_unchecked("")).to_str()))
     }
 );
 
@@ -45,7 +45,7 @@ methods!(
     fn is_ios(ua_string: RString) -> Boolean {
         let parser = Parser::new();
 
-        match parser.parse(ua_string.unwrap_or(RString::new("")).to_str()) {
+        match parser.parse(ua_string.unwrap_or(RString::new_usascii_unchecked("")).to_str()) {
             None => Boolean::new(false),
             Some(result) => {
                 match result.os {
@@ -66,7 +66,7 @@ methods!(
     fn is_android(ua_string: RString) -> Boolean {
         let parser = Parser::new();
 
-        match parser.parse(ua_string.unwrap_or(RString::new("")).to_str()) {
+        match parser.parse(ua_string.unwrap_or(RString::new_usascii_unchecked("")).to_str()) {
             None => Boolean::new(false),
             Some(result) => {
                 match result.os {
@@ -81,7 +81,7 @@ methods!(
 #[no_mangle]
 pub extern fn initialize_fast_woothee() {
     Class::new("FastWoothee", None).define(|itself| {
-        itself.const_set("VERSION", &RString::new("1.5.1").freeze());
+        itself.const_set("VERSION", &RString::new_usascii_unchecked("1.5.1").freeze());
         itself.def_self("parse", parse);
         itself.def_self("crawler?", is_crawler);
         itself.def_self("ios?", is_ios);
